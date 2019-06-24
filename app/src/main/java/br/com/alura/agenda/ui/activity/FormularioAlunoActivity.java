da5cd7a598604df8497bcfea4c6b50416f481c04
Maison <maisongalvao144@gmail.com>
2019-06-23 20:42:22 -0400

    Aula01 - Carregando aluno no formulário

--- app/src/main/java/br/com/alura/agenda/ui/activity/FormularioAlunoActivity.java
+++ app/src/main/java/br/com/alura/agenda/ui/activity/FormularioAlunoActivity.java
@@ -1,11 +1,14 @@
 package br.com.alura.agenda.ui.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+
+import java.io.Serializable;
 
 import br.com.alura.agenda.R;
 import br.com.alura.agenda.dao.AlunoDAO;
@@ -26,6 +29,16 @@ public class FormularioAlunoActivity extends AppCompatActivity {
         setTitle(TITULO_APPBAR);
         inicializacaoDosCampos();
         configuraBotaoSalvar();
+
+        Intent intent = getIntent();
+        Aluno aluno = (Aluno)intent.getSerializableExtra("ALUNO");
+        String nome = aluno.getNome();
+        String telefone = aluno.getTelefone();
+        String email = aluno.getEmail();
+        campoNome.setText(nome);
+        campoTelefone.setText(telefone);
+        campoEmail.setText(email);
+
     }
 
     private void configuraBotaoSalvar() {
