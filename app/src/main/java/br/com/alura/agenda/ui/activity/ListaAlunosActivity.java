0a0df749ed46ad529637727cd0166b11bd81903d
Maison <maisongalvao144@gmail.com>
2019-06-24 13:43:59 -0400

    Aula 04 - Aplicando menus do Android

--- app/src/main/java/br/com/alura/agenda/ui/activity/ListaAlunosActivity.java
+++ app/src/main/java/br/com/alura/agenda/ui/activity/ListaAlunosActivity.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
+import android.view.ContextMenu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -30,8 +32,15 @@ public class ListaAlunosActivity extends AppCompatActivity {
         setTitle(TITULO_APPBAR_ALUNO);
         configuraFabNovoAluno();
         configuraLista();
-        dao.salva(new Aluno("Maison","981840807","maisongalvao144@gmail.com"));
-        dao.salva(new Aluno("Nildes","98225412","nildes@gmail.com"));
+
+        dao.salva(new Aluno("Maison", "981840807", "maisongalvao144@gmail.com"));
+        dao.salva(new Aluno("Nildes", "98225412", "nildes@gmail.com"));
+    }
+
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+        super.onCreateContextMenu(menu, v, menuInfo);
+        getMenuInflater().inflate(R.menu.activity_lista_alunos_menu, menu);
     }
 
     private void configuraFabNovoAluno() {
@@ -64,18 +73,19 @@ public class ListaAlunosActivity extends AppCompatActivity {
         ListView listaDeAlunos = findViewById(R.id.activity_lista_alunos_listview);
         configuraAdapter(listaDeAlunos);
         configuraListenerDeCliquePorItem(listaDeAlunos);
-        configuralistenerDeCliqueLongoPorItem(listaDeAlunos);
+        registerForContextMenu(listaDeAlunos);
     }
 
-    private void configuralistenerDeCliqueLongoPorItem(ListView listaDeAlunos) {
-        listaDeAlunos.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
-            @Override
-            public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
-                Aluno alunoEscolhido = (Aluno) parent.getItemAtPosition(position);
-                remove(alunoEscolhido);
-                return true;
-            }
-        });
+    @Override
+    public boolean onContextItemSelected(MenuItem item) {
+        int itemId = item.getItemId();
+        if (itemId == R.id.activity_lista_alunos_remover) {
+            AdapterView.AdapterContextMenuInfo menuInfo = (AdapterView.AdapterContextMenuInfo) item.getMenuInfo();
+            Aluno alunoEscolhido = adapter.getItem(menuInfo.position);
+            remove(alunoEscolhido);
+            return super.onContextItemSelected(item);
+        }
+        return false;
     }
 
     private void remove(Aluno aluno) {
@@ -95,7 +105,7 @@ public class ListaAlunosActivity extends AppCompatActivity {
 
     private void abreFormularioModoEditaAluno(Aluno alunoEscolhido) {
         Intent vaiParaFormularioActivity = new Intent(ListaAlunosActivity.this, FormularioAlunoActivity.class);
-        vaiParaFormularioActivity.putExtra(CHAVE_ALUNO,alunoEscolhido);
+        vaiParaFormularioActivity.putExtra(CHAVE_ALUNO, alunoEscolhido);
         startActivity(vaiParaFormularioActivity);
     }
 
